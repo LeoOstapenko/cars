f91ef3fd1405833bb725f4abc94fc74d16785fde
leo_ostapenko <leoostapenko95@gmail.com>
2025-06-06 18:43:45 -0300

    descrição de venda do veículo gerada por ia da openai

--- cars/signals.py
+++ cars/signals.py
@@ -2,7 +2,7 @@ from django.db.models.signals import pre_save, post_save, post_delete
 from django.db.models import Sum
 from django.dispatch import receiver
 from cars.models import Car, CarInventory
-# from openai_api.client import get_car_ai_bio
+from openai_api.client import get_car_ai_bio
 
 def cars_inventory_update():
     cars_count = Car.objects.all().count() # Query no banco de dados atráves do ORM do Django.
@@ -17,11 +17,11 @@ def cars_inventory_update():
 @receiver(pre_save, sender=Car)
 def car_pre_save(sender, instance, **kwargs):
     if not instance.bio:
-        instance.bio = 'Sem informações.' # Caso não tenha mais bio gerada pela IA.
-        # ai_bio = get_car_ai_bio(
-        #     instance.model, instance.brand, instance.model_year
-        # )
-        # instance.bio = ai_bio
+        # instance.bio = 'Sem informações.' # Caso não tenha mais bio gerada pela IA.
+        ai_bio = get_car_ai_bio(
+            instance.model, instance.brand, instance.model_year
+        )
+        instance.bio = ai_bio
 
 @receiver(post_save, sender=Car)
 def car_post_save(sender, instance, **kwargs):
